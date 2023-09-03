27a38992a57ca4871609ebd8df51db0647a944d9
LyaFaridha <143927970+LyaFaridha@users.noreply.github.com>
2023-09-03 17:34:54 +0800

    Update AlyaAssignment.py

--- AlyaAssignment.py
+++ AlyaAssignment.py
@@ -1,51 +1,18 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
+import pandas as pd
 
 app = Dash(__name__)
 server = app.server
 
-app.title = "MCM7003 Data Visualization Interactive Demo" 
+app.title = "MCM7003 Data Visualization Interactive Demo"
 
-fig1 = px.line(x=["a","b","c","d","e"], y=[1,3,2,2,3], title="Sample figure")	
+# Load data from the provided URL
+data_url = "https://raw.githubusercontent.com/LyaFaridha/Assignment3/main/df_arabica_clean.csv"
+df = pd.read_csv(data_url)
 
-df = px.data.iris()
-fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
-
-app.layout = html.Div(
-    [html.H1("Data Visualization"),
-    dcc.RadioItems(id='my-radio', options=['Figure 1', 'Figure 2'], value='Figure 1', inline=True),
-    dcc.Graph(id='graph-output', figure ={})]
-)
-
-
-@app.callback(
-    Output(component_id='graph-output', component_property='figure'),
-    Input(component_id='my-radio', component_property='value')
-)
-
-def update_my_graph(val_chosen):
-    if (val_chosen == 'Figure 1'):
-        return fig1	
-    else: 
-        return fig2
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# Create a bar chart for the 'Country of Origin' counts
 country_counts = df['Country of Origin'].value_counts()
-fig = px.bar(country_counts, x=country_counts.index, y=country_counts.values)
-
-fig.update_layout(
-    xaxis_title = 'Country of Origin',
-    yaxis_title = 'Count',
-    title={'text' : 'Distribution of Country of Origins', 'x' : 0.5}
-)
-fig.show()
-
-import plotly.express as px
-
-df = px.data.gapminder()
-country_counts = df['country'].value_counts()
 fig_country_counts = px.bar(country_counts, x=country_counts.index, y=country_counts.values)
 
 fig_country_counts.update_layout(
@@ -54,49 +21,26 @@ fig_country_counts.update_layout(
     title={'text': 'Distribution of Country of Origins', 'x': 0.5}
 )
 
-from dash import Dash, html, dcc, Input, Output
-import plotly.express as px
-
-app = Dash(__name__)
-server = app.server
-
-app.title = "MCM7003 Data Visualization Interactive Demo" 
-
-fig1 = px.line(x=["a","b","c","d","e"], y=[1,3,2,2,3], title="Sample figure")
-
-df = px.data.iris()
-fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
-
-# Define the new figure
-df_gapminder = px.data.gapminder()
-country_counts = df_gapminder['country'].value_counts()
-fig_country_counts = px.bar(country_counts, x=country_counts.index, y=country_counts.values)
-
-fig_country_counts.update_layout(
-    xaxis_title='Country of Origin',
-    yaxis_title='Count',
-    title={'text': 'Distribution of Country of Origins', 'x': 0.5}
+# Create the first scatter plot
+fig1 = px.scatter(df, x='Aroma', y='Flavor', color='Country of Origin',
+                 labels={'Aroma': 'Aroma', 'Flavor': 'Flavor', 'Country of Origin': 'Country of Origin'},
+                 title='Relationship between Aroma and Flavor Attributes by Country of Origin')
+fig1.update_traces(
+    marker=dict(size=10),
+    selector=dict(mode='markers')
 )
+
+# Create the second box plot
+fig2 = px.box(df, x='Processing Method', y=['Aroma', 'Flavor'],
+             labels={'variable': 'Attribute', 'value': 'Rating', 'Processing Method': 'Processing Method'},
+             title='Comparison of Aroma and Flavor Attributes across Processing Methods')
 
 app.layout = html.Div(
     [html.H1("Data Visualization"),
-     dcc.RadioItems(id='my-radio', options=['Figure 1', 'Figure 2', 'Country Counts'], value='Figure 1', inline=True),
-     dcc.Graph(id='graph-output', figure={})]
+     dcc.Graph(id='graph-output', figure=fig_country_counts),
+	dcc.Graph(id='graph1-output', figure=fig1),
+        dcc.Graph(id='graph2-output', figure=fig2)]
 )
-
-
-@app.callback(
-    Output(component_id='graph-output', component_property='figure'),
-    Input(component_id='my-radio', component_property='value')
-)
-def update_my_graph(val_chosen):
-    if val_chosen == 'Figure 1':
-        return fig1
-    elif val_chosen == 'Figure 2':
-        return fig2
-    elif val_chosen == 'Country Counts':
-        return fig_country_counts
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
